dd97f4db8259039c53aecffb4b89430e1039b4fd
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2025-02-17 14:23:05 +0400

    Defined _UNICODE and placed globals in struct.

--- src/medify.c
+++ src/medify.c
@@ -1,23 +1,27 @@
 #include "medify.h"
 
+APP_STATE app;
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
     PWSTR szCmdLine, int iCmdShow)
 {
     WNDCLASSEXW wcex;
 
-    wchar_t szClassName[] = L"medify";
+    app.szClassName = L"medify";
+    app.hInstance = hInstance;
+    app.lpfnWndProc = WndProc;
 
     wcex.cbSize = sizeof(wcex);
     wcex.style  = CS_VREDRAW | CS_HREDRAW;
-    wcex.lpfnWndProc = WndProc;
+    wcex.lpfnWndProc = app.lpfnWndProc;
     wcex.cbClsExtra = 0;
     wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
+    wcex.hInstance = app.hInstance;
     wcex.hIcon = LoadIconW(NULL, IDI_APPLICATION);
     wcex.hCursor = LoadCursorW(NULL, IDC_ARROW);
     wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
     wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = szClassName;
+    wcex.lpszClassName = app.szClassName;
     wcex.hIconSm = NULL;
 
     if (!RegisterClassExW(&wcex))
@@ -26,20 +30,30 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
         return 1;
     }
 
-    // CreateWindowExW(
-    //     0,
-    //     szClassName,
-    //     [in, optional] LPCWSTR   lpWindowName,
-    //     [in]           DWORD     dwStyle,
-    //     [in]           int       X,
-    //     [in]           int       Y,
-    //     [in]           int       nWidth,
-    //     [in]           int       nHeight,
-    //     [in, optional] HWND      hWndParent,
-    //     [in, optional] HMENU     hMenu,
-    //     [in, optional] HINSTANCE hInstance,
-    //     [in, optional] LPVOID    lpParam
-    // );
+    CreateWindowExW(
+        0,
+        app.szClassName,
+        L"Medify",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    MSG msg;
+
+    while (GetMessageW(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+
+    return msg.wParam;
 
     // open a dialog box at specifc time (6 pm)
 }
--- src/medify.h
+++ src/medify.h
@@ -2,7 +2,19 @@
 #define UNICODE
 #endif
 
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
 #include <windows.h>
+#include <stdio.h>
+
+typedef struct APP_STATE
+{
+    wchar_t *szClassName;
+    HINSTANCE hInstance;
+    WNDPROC lpfnWndProc;
+} APP_STATE;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, 
     WPARAM wParam, LPARAM lParam);
